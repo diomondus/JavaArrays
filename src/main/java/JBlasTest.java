1777ef2c2e4cca91ae03dc40a1b8e98be7aa246a
Dmitry Butilov <acebutilov@gmail.com>
2019-05-22 21:28:54 +0300

    Удаляет тестовые классы

---
+++ src/main/java/JBlasTest.java
@@ -0,0 +1,24 @@
+import org.jblas.DoubleMatrix;
+
+public class JBlasTest {
+
+    public static void main(String[] args) {
+        processJBLASMatrix();
+    }
+
+    public static void processJBLASMatrix() {
+        DoubleMatrix a = DoubleMatrix.randn(2000, 2000);
+        DoubleMatrix b = DoubleMatrix.randn(2000, 2000);
+//        DoubleMatrix a = DoubleMatrix.randn(2, 2);
+//        DoubleMatrix b = DoubleMatrix.randn(2, 2);
+//        a.print();
+//        b.print();
+        Long time = System.currentTimeMillis();
+        System.out.println("mul start");
+        DoubleMatrix c = a.mmul(b);
+//        c.print();
+        System.out.println("mul end: " + ((System.currentTimeMillis() - time) / 1000.0) + "c");
+        DoubleMatrix d = b.add(c);
+//        d.print();
+    }
+}
--- src/main/java/MatrixOperator.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Dmitry Butilov
- * on 13.07.17.
- */
-public class MatrixOperator {
-}
